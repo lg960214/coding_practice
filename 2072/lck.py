e5b5d4296cff7b9807ccfea35764a2b00b3592c3
cklee94 <briancklee94@gmail.com>
2022-03-11 01:11:34 +0000

    2072

--- 2072/lck.py
+++ 2072/lck.py
@@ -20,25 +20,25 @@ for i in range(n):
     if i % 2 == 0:  # black
         black.append(list(map(int, input().split())))
         x, y = black[-1]
-        for direction in directions:
-            count = 1
-            for direc in direction:
-                a, b = direc
-                dfs(x, y, a, b, black)
-            if count == 5:
-                if answer == -1:
+        if answer == -1:
+            for direction in directions:
+                count = 1
+                for direc in direction:
+                    a, b = direc
+                    dfs(x, y, a, b, black)
+                if count == 5:
                     answer = i + 1
 
     else:  # white
         white.append(list(map(int, input().split())))
         x, y = white[-1]
-        for direction in directions:
-            count = 1
-            for direc in direction:
-                a, b = direc
-                dfs(x, y, a, b, white)
-            if count == 5:
-                if answer == -1:
+        if answer == -1:
+            for direction in directions:
+                count = 1
+                for direc in direction:
+                    a, b = direc
+                    dfs(x, y, a, b, white)
+                if count == 5:
                     answer = i + 1
 
 print(answer)
