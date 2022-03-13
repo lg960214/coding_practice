bdd6e3d854f47e57e198d79c4e09d7b6241efc86
cklee94 <briancklee94@gmail.com>
2022-03-14 08:38:27 +0900

    1002

---
+++ 1002/1002_lck.py
@@ -0,0 +1,20 @@
+T = int(input())
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = list(map(int, input().split()))
+    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    summation = r1 + r2
+    if x1 == x2 and y1 == y2:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+    elif distance + r1 == r2 or distance + r2 == r1:
+        print(1)
+    elif distance + r1 < r2 or distance + r2 < r1:
+        print(0)
+    elif distance == summation:
+        print(1)
+    elif distance < summation:
+        print(2)
+    elif distance > summation:
+        print(0)
---
+++ 1002/README.md
@@ -0,0 +1,5 @@
+# 1002: 터렛
+
+- 분류: 수학, 기하학
+- 난이도: 실버 4
+- [https://www.acmicpc.net/problem/1002](https://www.acmicpc.net/problem/1002)
