ff0b0b5a8e2b87c3089f106ddc150ede0fdf6e56
cklee94 <briancklee94@gmail.com>
2022-04-25 08:56:16 +0900

    3495

---
+++ 3495/3495_lck.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+h, w = map(int, input().strip().split())
+data = [input().strip() for _ in range(h)]
+
+answer = 0
+check = 0
+
+for i in range(h):
+    for j in range(w):
+        if data[i][j] == "/" or data[i][j] == "\\":
+            slash = j
+            answer += 0.5
+            check += 1
+        elif check % 2:
+            answer += 1
+
+print(int(answer))
