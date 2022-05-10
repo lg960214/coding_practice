ef46a45ee010571ad91a5c8a794d0ad2944be23f
hyucksukim <86274940+hyucksukim@users.noreply.github.com>
2022-05-10 10:51:16 +0900

    Add files via upload

---
+++ 17215/17215_khs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a = input()
+
+answer = []
+ind = 0
+
+def score(x):
+    if x == 'S':
+        return 10
+    elif x == 'P':
+        return 10
+    elif x == '-':
+        return 0
+    else:
+        return int(x)
+
+for i in range(len(a)):
+    now = a[i]
+    if now == 'S':
+        if a[i+2] != 'P':
+            answer.append((10 + score(a[i+1])+score(a[i+2])))
+        else:
+            answer.append(20)
+        ind += 1
+    elif now == 'P':
+        answer[ind] = (10 + score(a[i+1]))
+        ind += 1
+    else:
+        if len(answer) == ind:
+            answer.append(score(now))
+        else:
+            answer[ind] += score(now)
+            ind += 1
+    if ind == 10:
+        break  
+print(sum(answer))
+
