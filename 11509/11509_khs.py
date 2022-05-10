b117572d8e2d706bbb9a54fe27bd7ce75b89a5b0
hyucksukim <86274940+hyucksukim@users.noreply.github.com>
2022-05-10 12:27:29 +0900

    Add files via upload

---
+++ 11509/11509_khs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n = int(input())
+h = list(map(int, input().split()))
+
+answer = 0
+h_list = [0]*1000001
+
+for i in h:
+    if h_list[i] == 0:
+        h_list[i-1] += 1
+        answer += 1
+    else:
+        h_list[i] -= 1
+        h_list[i-1] += 1
+
+print(answer)
+
