560ff6ffb84ce6cf2c80fae4f2a9f5f2b23e8f17
이건 <46098797+lg960214@users.noreply.github.com>
2024-07-22 16:05:58 +0900

    [Bronze V] Title: 시험 성적, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+target = int(input())
+
+if (100 >= target >= 90):
+    print("A")
+elif (80 <= target <= 89):
+    print("B")
+elif (70 <= target <= 79):
+    print("C")
+elif (60 <= target <= 69):
+    print("D")
+else:
+    print("F")
