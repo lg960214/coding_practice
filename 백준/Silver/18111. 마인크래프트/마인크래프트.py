08ff0027f2302b6bf1288b6007e7c52fc164e29c
이건 <46098797+lg960214@users.noreply.github.com>
2024-07-26 20:32:04 +0900

    [Silver II] Title: 마인크래프트, Time: 704 ms, Memory: 112636 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,60 +1,30 @@
-class Sol:
-    answer = 1e9
-    height = -1
+land = []
 
-    def __init__(self):
-        self._initialize()
-        self._input()
+N, M, B = map(int, input().split())
+min_height = 0
+max_height = 0
+for _ in range(N):
+    temp = list(map(int, input().split()))
+    max_height += sum(temp)
+    land.append(temp)
 
-        for threshold in range(self.min_, self.max_+1):
-            if not (self.ifPossible(threshold)): continue
+max_height += B
+max_height //= N*M
 
-            time = self.calculateTime(threshold)
-            if (self.answer >= time):
-                self.height = threshold
-                self.answer = time
+def mine_time(land, height, inven):
+    time = 0
+    for i in range(N):
+        for j in range(M):
+            if land[i][j] > height:
+                time += (land[i][j] - height) * 2
+            elif land[i][j] < height:
+                time += height - land[i][j]
+    return time
+min_time = 1e9
+for i in range(min_height, max_height+1):
+    i_time = mine_time(land, i, B)
+    if i_time <= min_time:
+        min_time = i_time
+        min_height_result = i
 
-    def calculateTime(self, thr: int) -> int:
-        t_in = 2
-        t_out = 1
-
-        cal_t = 0
-        for i in range(self.N):
-            for j in range(self.M):
-                target = self.arr[i][j] - thr
-                if (target < 0):
-                    cal_t -= target*t_out
-                elif (target > 0):
-                    cal_t += target*t_in
-
-        return cal_t
-
-
-    def ifPossible(self, thr: int) -> bool:
-        temp_sum = self.num_block - self.floor_size*thr + self.B
-        
-        if (0 <= temp_sum):
-            return True
-
-    def _initialize(self):
-        self.arr = []
-        self.min_ = 256
-        self.max_ = 0
-
-    def _input(self):
-        self.N, self.M, self.B = map(int, input().split())
-
-        for i in range(self.N):
-            temp_lst = list(map(int, input().split()))
-            self.min_ = min(self.min_, min(temp_lst))
-            self.max_ = max(self.max_, max(temp_lst))
-            self.arr.append(temp_lst)
-
-        self.floor_size = self.N * self.M
-
-        self.num_block = 0
-        for i in range(self.N):
-            self.num_block += sum(self.arr[i])
-
-user = Sol()
-print(user.answer, user.height)
+print(min_time, min_height_result)
