ccd1ffad8022752ba6caecac56866a34d3e40329
이건 <46098797+lg960214@users.noreply.github.com>
2024-07-29 15:42:04 +0900

    [Silver II] Title: 스택 수열, Time: 216 ms, Memory: 127336 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -8,13 +8,10 @@ result = []
 for i in range(1, n+1):
     stk.append(i)
     result.append('+')
-    while stk and idx < n:
-        if (nums[idx] == stk[-1]):
-            stk.pop()
-            result.append('-')
-            idx += 1
-        else:
-            break
+    while stk and (nums[idx] == stk[-1]):
+        stk.pop()
+        result.append('-')
+        idx += 1
 
 if not stk:
     for i in range(len(result)):
