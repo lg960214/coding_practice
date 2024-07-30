89f001d3b43bc519a6cb6a58cd0a327d77801c03
이건 <46098797+lg960214@users.noreply.github.com>
2024-07-30 13:38:00 +0900

    [Silver IV] Title: 스택 2, Time: 476 ms, Memory: 180324 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -3,28 +3,37 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-stack_ = []
-
+stk = []
 for _ in range(N):
-    temp = input().rstrip().split()
-    if (len(temp) == 2):
-        stack_.append(temp[1])
+    temp = input().rstrip()
+    if len(temp) == 1:
+        rule = int(temp)
     else:
-        if (temp[0] == '2'):
-            if (len(stack_) != 0):
-                print(stack_.pop())
-            else:
-                print(-1)
-        if (temp[0] == '3'):
-            print(len(stack_))
-        if (temp[0] == '4'):
-            if (len(stack_) == 0):
-                print(1)
-            else:
-                print(0)
-        if (temp[0] == '5'):
-            if (len(stack_) != 0):
-                print(stack_[-1])
-            else:
-                print(-1)
+        rule, x = map(int, temp.split())
+    if rule == 1:
+        stk.append(x)
+        continue
+    elif rule == 2:
+        if stk:
+            print(stk.pop())
+            continue
+        else:
+            print(-1)
+            continue
+    elif rule == 3:
+        print(len(stk))
+        continue
+    elif rule == 4:
+        if not stk:
+            print(1)
+            continue
+        else:
+            print(0)
+            continue
+    else:
+        if stk:
+            print(stk[-1])
+            continue
+        else:
+            print(-1)
+            continue
