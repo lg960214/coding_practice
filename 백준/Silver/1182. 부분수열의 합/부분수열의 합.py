9285f441b2cc0e1656393cbfc893d10c26fbd75b
이건 <46098797+lg960214@users.noreply.github.com>
2024-08-06 14:33:28 +0900

    [Silver II] Title: 부분수열의 합, Time: 340 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -4,6 +4,9 @@ arr = list(map(int, input().split()))
 
 answer = 0
 
+# 2진법 활용
+# i가 2진법 수의 자리수를 순회하며 1인 경우에만 더하도록 구성
+# ex) 01100 -> i가 2, 3일 때의 부분집합의 합을 구함
 for bm in range(1, 1<<N):
     sum_ = 0
     for i in range(N):
