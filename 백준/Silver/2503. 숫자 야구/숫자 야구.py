707eba48ca5f4b7e504db0924dd6739035cf52bf
이건 <46098797+lg960214@users.noreply.github.com>
2024-08-12 19:18:51 +0900

    [Silver III] Title: 숫자 야구, Time: 104 ms, Memory: 111000 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,31 +1,30 @@
-class Sol:
-    number_list = []
-    def __init__(self):
-        for i in '123456789':
-            for j in '123456789':
-                for k in '123456789':
-                    if ((i==j) or (j==k) or (k==i)): continue
-                    self.number_list.append(i+j+k)
+number_list = []
 
-        self.N = int(input())
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            if i==j or j==k or k==i: continue
+            number_list.append(str(i)+str(j)+str(k))
 
-        for _ in range(self.N):
-            target, s, b = map(str, input().split())
-            temp_list = []
+N = int(input())
 
-            for comp in self.number_list:
-                s_cnt, b_cnt = 0, 0
-                for i in range(3):
-                    if (comp[i] == target[i]):
-                        s_cnt += 1
-                    elif (target[i] in comp):
-                        b_cnt += 1
-                if ((s_cnt == int(s)) and (b_cnt == int(b))):
-                    temp_list.append(comp)
+for _ in range(N):
+    target, s, b = map(str, input().split())
 
-            self.number_list = temp_list
+    compare_lst = []
 
-        print(len(self.number_list))
+    for comp in number_list:
+        s_cnt, b_cnt = 0, 0
 
+        for i in range(3):
+            if comp[i] == target[i]:
+                s_cnt += 1
+            elif target[i] in comp:
+                b_cnt += 1
 
-user = Sol()
+        if s_cnt == int(s) and b_cnt == int(b):
+            compare_lst.append(comp)
+
+    number_list = compare_lst
+
+print(len(number_list))
