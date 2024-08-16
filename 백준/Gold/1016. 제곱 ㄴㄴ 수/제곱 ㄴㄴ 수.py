346c46d71ec85b106daabab04bceb8d99f2724e9
이건 <46098797+lg960214@users.noreply.github.com>
2024-08-16 12:38:50 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 744 ms, Memory: 80784 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -0,0 +1,35 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+
+class Sol:
+    def __init__(self):
+        min_, max_ = map(int, input().split())
+
+        array = [0]*(max_-min_+1)
+        exp = []
+
+        for i in range(2, int(math.sqrt(max_))+1):
+            exp.append(i**2)
+
+        for nn in exp:
+            gap = min_ % nn
+
+            if gap == 0:
+                idx = gap
+            else:
+                idx = nn - gap
+
+            while idx <= max_-min_:
+                array[idx] = 1
+                idx += nn
+        
+
+        print(array.count(0))
+
+
+
+if __name__ == '__main__':
+    user = Sol()
